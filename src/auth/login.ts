c0860b5f8e9fa6f99d7ae8ed07413b3cc306bd4c
Levi <litbelb1@outlook.com>
2022-02-24 15:42:16 -0800

    login updates

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -4,15 +4,20 @@ import { hashString } from "../utils/hash";
 import userSchema from "../models/user";
 
 const login = async (req: express.Request, res: express.Response) => {
-    let body = req.body;
-    const user = mongoose.model("users", userSchema);
-    const resp = await user.find({ username: body.username });
-    const [salt, key] = resp[0].password.split(":");
-    const hashedPassword = await hashString(body.password)
-    if (hashedPassword === key) {
-        res.send("success!")
-    } else {
-        res.send("screw you")
+    try {
+        let body = req.body;
+        const user = mongoose.model("users", userSchema);
+        const resp = await user.find({ username: body.username });
+        const [salt, key] = resp[0].password.split(":");
+        const hashedPassword = await hashString(body.password)
+        if (hashedPassword === key) {
+            res.sendStatus(200).json({ response: "Success!" })
+        } else {
+            res.sendStatus(401).json({ response: `Error: Invalid password!`, errorCode: "0x00610", resolution: "Double-check your password and make sure it's right." })
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500).json({ response: `Error: ${err}`, errorCode: "0x11600", resolution: "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet" });
     }
 };
 
