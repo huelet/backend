5adbb0ec985eefb6624a6f8b0e9208945fe31220
Levi <litbelb1@outlook.com>
2022-03-23 03:30:29 -0700

    Update model types & defaults

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,14 +4,15 @@ const userSchema = new Schema({
     username: String,
     uid: String,
     email: String,
+    phoneNumber: Number,
     password: String,
     bio: { type: String, default: "sussy amogus impostor" },
     pfp: { type: String, default: "https://cdn.huelet.net/assets/AvatarMenu_defaultAvatarSmall.png" },
     location: { type: String, default: "The vent" },
     private: Boolean,
-    creator: { Boolean, default: false },
-    approved: { Boolean, default: false },
-    followers: Number,
+    creator: { type: Boolean, default: false },
+    approved: { type: Boolean, default: false },
+    followers: { type: Number, default: 0 },
     userCreated: String,
     userAuth: String
 })
