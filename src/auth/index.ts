b2ca822b4ab3676b73ac85f6808ce4226a37496a
Levi <litbelb1@outlook.com>
2022-03-24 00:02:22 -0700

    ✨ Privacy toggle route

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,10 +1,12 @@
 import signup from "./signup";
 import login from "./login";
+import setPrivacy from "./settings/privacy";
 import userLookup from "./user";
 
 
 export {
     signup,
     login,
-    userLookup
+    userLookup,
+    setPrivacy
 };
---
+++ src/auth/settings/privacy.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import userSchema from "../../models/user";
+
+const setPrivacy = async (req: express.Request, res: express.Response) => {
+  try {
+    let body: any = req.body;
+    let privacyStatus = body.privacyStatus;
+    const user: mongoose.Model<any, {}, {}, {}> = mongoose.model("users", userSchema);
+    const resp: any[] = await user.find({ username: body.username });
+    if (typeof privacyStatus !== "boolean") {
+        res.status(400).json({
+            success: false,
+            message: "Invalid option. Stop being naughty."
+        });
+        return;
+    } else {
+      await user.updateOne(
+        { username: resp[0].username },
+        {
+          private: privacyStatus,
+        }
+      );
+      res.status(200).json({
+        success: true,
+      });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      response: `Error: ${err}`,
+      errorCode: "0x11600",
+      resolution:
+        "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet",
+    });
+  }
+};
+
+export default setPrivacy;
