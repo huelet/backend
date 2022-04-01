85ed0103fe14430d43accd73b3a6912993e5b773
Levi <litbelb1@outlook.com>
2022-03-31 22:18:21 -0700

    Delete document after use

--- src/auth/authVerify.ts
+++ src/auth/authVerify.ts
@@ -7,32 +7,40 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 const verifyAuth = async (req: express.Request, res: express.Response) => {
-    try {
-        let body = req.body;
-        let jwtSecret: any | any = process.env.JWT_SECRET;
-        const auth = mongoose.model("verification", verifySchema);
-        const resp = await auth.findOne({ authId: body.authId });
-        if (resp) {
-            if (resp.authCode === body.authCode) {
-                const token = jwt.sign({ username: resp[0].username }, jwtSecret, {
-                  expiresIn: "31d",
-                });
-                res.status(200).json({ response: "Success!", token: token });
-                return;
-            } else {
-                res.status(401).json({
-                    response: `Error: Invalid auth code!`,
-                    errorCode: "0x00610",
-                    resolution: "Double-check your auth code and make sure it's right.",
-                });
-                return;
-            }
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ response: `Error: ${err}`, errorCode: "0x11600", resolution: "Wait 15-20 minutes and try again. If it's not fixed, report a bug in the #bug-reports channel on discord" });
+  try {
+    let body = req.body;
+    let jwtSecret: any | any = process.env.JWT_SECRET;
+    const auth = mongoose.model("verification", verifySchema);
+    const resp = await auth.findOne({ authId: body.authId });
+    if (resp) {
+      if (resp.authCode === body.authCode) {
+        const resp = await auth.deleteOne({ authId: body.authId });
+        const token = jwt.sign({ username: resp[0].username }, jwtSecret, {
+          expiresIn: "31d",
+        });
+        res.status(200).json({ response: "Success!", token: token });
         return;
+      } else {
+        res.status(401).json({
+          response: `Error: Invalid auth code!`,
+          errorCode: "0x00610",
+          resolution: "Double-check your auth code and make sure it's right.",
+        });
+        return;
+      }
     }
-}
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({
+        response: `Error: ${err}`,
+        errorCode: "0x11600",
+        resolution:
+          "Wait 15-20 minutes and try again. If it's not fixed, report a bug in the #bug-reports channel on discord",
+      });
+    return;
+  }
+};
 
 export default verifyAuth;
