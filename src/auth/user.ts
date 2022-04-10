7c390202b7b4aba20c2cc8d369285e0db161e10e
Levi <litbelb1@outlook.com>
2022-04-09 23:20:40 -0700

    fix user lookup

--- src/auth/user.ts
+++ src/auth/user.ts
@@ -3,23 +3,24 @@ import mongoose from "mongoose";
 import userSchema from "../models/user";
 
 const userLookup = async (req: express.Request, res: express.Response) => {
-    try {
-        let params = req.params;
-        const user = mongoose.model("users", userSchema);
-        if (params.username) {
-            const resp = await user.find({ username: params.username });
-            res.status(200).json(resp[0]);
-            return;
-        } else if (params.uid) {
-            const resp = await user.find({ uid: params.uid });
-            res.status(200).json(resp[0]);
-            return;
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ response: `Error: ${err}`, errorCode: "0x11600", resolution: "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet" });
-        return;
-    }
-}
+  try {
+    let params = req.params;
+    const user = mongoose.model("users", userSchema);
+    const resp = await user.find({ username: params.username });
+    res.status(200).json(resp[0]);
+    return;
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({
+        response: `Error: ${err}`,
+        errorCode: "0x11600",
+        resolution:
+          "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet",
+      });
+    return;
+  }
+};
 
 export default userLookup;
