c152b8813af5f62f4f1b653b46d3fdcef37eb778
Levi <litbelb1@outlook.com>
2022-06-21 10:19:45 -0700

    ✨ send thumbnail to video lookup endpoit

--- src/videos/videoGet.ts
+++ src/videos/videoGet.ts
@@ -3,29 +3,37 @@ import mongoose from "mongoose";
 import videoSchema from "../models/video";
 
 const getVideoInfo = async (req: express.Request, res: express.Response) => {
-    try {
-        let { vuid } = req.params;
-        const video = mongoose.model("videos", videoSchema);
-        const resp = await video.find({ vuid: vuid });
-        const data = resp[0];
-        res.status(200).json({
-            vtitle: data.title,
-            vurl: data.url,
-            vdesc: data.description,
-            vauthor: data.authorId,
-            vdate: data.videoUploaded,
-            vviews: data.views,
-            vclaps: data.upvotes,
-            vcraps: data.downvotes,
-            vshares: data.shares,
-            vcomments: data.comments,
-        })
-        return;
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ response: `Error: ${err}`, errorCode: "yes", resolution: "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet" });
-        return
-    }
-}
+  try {
+    let { vuid } = req.params;
+    const video = mongoose.model("videos", videoSchema);
+    const resp = await video.find({ vuid: vuid });
+    const data = resp[0];
+    res.status(200).json({
+      vtitle: data.title,
+      vurl: data.url,
+      vdesc: data.description,
+      vauthor: data.authorId,
+      vdate: data.videoUploaded,
+      vviews: data.views,
+      vclaps: data.upvotes,
+      vcraps: data.downvotes,
+      vshares: data.shares,
+      vcomments: data.comments,
+      vimg: data.thumbnail,
+    });
+    return;
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({
+        response: `Error: ${err}`,
+        errorCode: "yes",
+        resolution:
+          "Wait 15-20 minutes and try again. If it's not fixed, report a bug by pinging us on twitter @TeamHuelet",
+      });
+    return;
+  }
+};
 
 export { getVideoInfo };
