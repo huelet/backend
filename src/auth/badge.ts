36da16ea39f8b0ec3d0a2442eb78588eb22f7f95
Levi R <levir620@skiff.com>
2022-07-12 09:19:38 -0700

    🔥 remove old code

--- src/auth/badge.ts
+++ src/auth/badge.ts
@@ -5,33 +5,6 @@ import userSchema from "../models/user";
 const canvas = createCanvas(512, 1024);
 const ctx = canvas.getContext("2d");
 
-const createBadge = async (username, pfpUrl, timeCreated) => {
-  const backgroundGradient = await loadImage(
-    "https://cdn.huelet.net/assets/background%20gradient.png"
-  );
-  const avatar = await loadImage(pfpUrl);
-  const logo = await loadImage(
-    "https://cdn.huelet.net/assets/logo-transparent.png"
-  );
-  const timestamp = new Date(timeCreated * 1000);
-  const formattedDate = `${timestamp.getDate()}.${timestamp.getMonth()}.${timestamp.getFullYear()}`;
-  ctx.font = "64px Arial";
-  ctx.fillStyle = "rgba(0, 0, 0, 1)";
-  ctx.strokeStyle = "rgba(0, 0, 0, 1)";
-  ctx.drawImage(backgroundGradient, -100, 0, 2048, 1024);
-  ctx.drawImage(logo, 290, 800, 256, 256);
-  ctx.drawImage(avatar, 64, 100, 384, 384);
-  ctx.fillText(username.toUpperCase(), 64, 554);
-  ctx.fillText("Creator", 64, 620);
-  ctx.font = `32px Arial`;
-  ctx.fillText(formattedDate, 80, 935);
-  ctx.strokeRect(64, 900, 165, 52);
-
-  const stream = canvas.createPNGStream();
-};
-
-createBadge("huelet", "https://cdn.huelet.net/assets/logo.png", 1656999249.166);
-
 const badge = async (req: express.Request, res: express.Response) => {
   try {
     let date: any = new Date();
